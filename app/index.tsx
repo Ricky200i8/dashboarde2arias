91f4dbab3d024418c90282b894e293d1e18689db
ricardoariasgal87 <ricardoariasgal87@gmail.com>
2025-09-17 16:48:45 +0000

    ✨feat(components): Crear el componente ProfileDetails - Crear la carpeta components - Crear el componente ProfileDetails.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
+import ProfileDetails from "@/components/ProfileDetails";
 import "../global.css"
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-800">
-        Welcome to Nativewind!
+      <ProfileDetails></ProfileDetails>
       </Text>
     </View>
   );
---
+++ components/ProfileDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileDetails= () => {
+  return (
+    <View>
+      <Text>profile-delails</Text>
+    </View>
+  )
+}
+
+export default ProfileDetails
