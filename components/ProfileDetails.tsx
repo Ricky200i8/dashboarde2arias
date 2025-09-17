c89f98128f471304c20071cddc3e19991d40cd69
ricardoariasgal87 <ricardoariasgal87@gmail.com>
2025-09-17 17:37:51 +0000

    💄ui(ProfileDetails): Crear los detalles principales del usuario. - Añadir imagen de perfil. - Añadir nombre y profecion del usuario.

--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
+import "@/global.css"
 import React from 'react'
+import { Image, View, Text } from 'react-native'
 
 const ProfileDetails= () => {
   return (
-    <View>
-      <Text>profile-delails</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Image className='max-h-24 max-w-24' source={require('../assets/images/icon.png')} ></Image>
+      <Text className="text-2xl font-bold">Ricardo Arias</Text>
+      <Text className="text-xl font-bold text-gray-400">Estudiante</Text>
     </View>
+    
   )
 }
 
